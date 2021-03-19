6bfb2debc6c9563e7937b7de2ff6925d8cc19027
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-03-19 09:32:21 +0000

    Changed Title

--- add.php
+++ add.php
@@ -14,7 +14,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>PHP: CRUD (Add, Edit, Delete, View) Application using OOP (Object Oriented Programming) and MYSQL</title>
+  <title>PHP: CRUD Application using OOP and MYSQL</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
